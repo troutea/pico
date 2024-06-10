3e0bafbc3e5def7ef3dd4f36470d12a91018a699
troutea <troutea@yahoo.co.uk>
2024-06-10 14:33:20 +0100

    adding my pico code

---
+++ MyLearning1_vscode/build/pioasm/CMakeFiles/pioasm.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/agtrout/pico/pico-sdk/tools/pioasm/ada_output.cpp" "/home/agtrout/pico/development_pico/MyLearning1_vscode/build/pioasm/CMakeFiles/pioasm.dir/ada_output.cpp.o"
+  "/home/agtrout/pico/pico-sdk/tools/pioasm/c_sdk_output.cpp" "/home/agtrout/pico/development_pico/MyLearning1_vscode/build/pioasm/CMakeFiles/pioasm.dir/c_sdk_output.cpp.o"
+  "/home/agtrout/pico/pico-sdk/tools/pioasm/gen/lexer.cpp" "/home/agtrout/pico/development_pico/MyLearning1_vscode/build/pioasm/CMakeFiles/pioasm.dir/gen/lexer.cpp.o"
+  "/home/agtrout/pico/pico-sdk/tools/pioasm/gen/parser.cpp" "/home/agtrout/pico/development_pico/MyLearning1_vscode/build/pioasm/CMakeFiles/pioasm.dir/gen/parser.cpp.o"
+  "/home/agtrout/pico/pico-sdk/tools/pioasm/hex_output.cpp" "/home/agtrout/pico/development_pico/MyLearning1_vscode/build/pioasm/CMakeFiles/pioasm.dir/hex_output.cpp.o"
+  "/home/agtrout/pico/pico-sdk/tools/pioasm/main.cpp" "/home/agtrout/pico/development_pico/MyLearning1_vscode/build/pioasm/CMakeFiles/pioasm.dir/main.cpp.o"
+  "/home/agtrout/pico/pico-sdk/tools/pioasm/pio_assembler.cpp" "/home/agtrout/pico/development_pico/MyLearning1_vscode/build/pioasm/CMakeFiles/pioasm.dir/pio_assembler.cpp.o"
+  "/home/agtrout/pico/pico-sdk/tools/pioasm/pio_disassembler.cpp" "/home/agtrout/pico/development_pico/MyLearning1_vscode/build/pioasm/CMakeFiles/pioasm.dir/pio_disassembler.cpp.o"
+  "/home/agtrout/pico/pico-sdk/tools/pioasm/python_output.cpp" "/home/agtrout/pico/development_pico/MyLearning1_vscode/build/pioasm/CMakeFiles/pioasm.dir/python_output.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/agtrout/pico/pico-sdk/tools/pioasm"
+  "/home/agtrout/pico/pico-sdk/tools/pioasm/gen"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
